ce1b9034594a57dc73972b8e937e7e36cc7fb087
hfeng620-cmd <hfeng620@gmail.com>
2026-06-25 07:31:57 +0800

    修复profile重复import

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useCallback, useEffect, useState } from "react";
-
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 import { AuthButton } from "@/components/auth-button";
